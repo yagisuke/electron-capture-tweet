7d745d6b9bc5bcc3dad5ef0b89ceb799b7cf04db
yagisuke <yagisuke.0209@gmail.com>
2018-04-29 18:22:43 +0900

    create cutting function

---
+++ src/main/trimDesktop.js
@@ -0,0 +1,30 @@
+import { screen, ipcMain, BrowserWindow } from 'electron'
+
+function trimDesktop() {
+    const displays = screen.getAllDisplays()
+    return new Promise((resolve, reject) => {
+        const windows = displays.map((display, i) => {
+            const { x, y, width, height } = display.bounds
+            display.name = 'Screen ' + (i + 1)
+            const win = new BrowserWindow({
+                frame: false,
+                transparent: true,
+                alwaysOnTop: true,
+                x, y, width, height
+            })
+
+            win.loadURL(`file://${__dirname}/../../index.html`)
+
+            return { win, display }
+        })
+
+        ipcMain.once('SEND_BOUNDS', (e, { trimmendBounds }) => {
+            const sourceDisplay = windows.find(w => w.win.webContents.id === e.sender.id).display
+            const profile = { sourceDisplay, trimmendBounds }
+            windows.forEach(w => w.win.close())
+            resolve(profile)
+        })
+    })
+}
+
+export default trimDesktop
