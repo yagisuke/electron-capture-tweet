c3e01d66d004e823435992fc6678a451197a3334
yagisuke <yagisuke.0209@gmail.com>
2018-04-30 16:52:43 +0900

    twitter settings

---
+++ src/const/index.js
@@ -0,0 +1,7 @@
+import { default as TWITTER_CONFIG } from './twitter-config'
+
+const Const = {
+  TWITTER_CONFIG
+}
+
+export default Const
---
+++ src/const/twitter-config.js
@@ -0,0 +1,9 @@
+/**
+ * consumerKeyとconsumerSecretには、
+ * twitter[https://apps.twitter.com/] に登録したアプリケーション設定画面の
+ * 「Keys and Access Tokens」 タブに記載されている 「Consumer Key」「Consumer Secret」を指定する.
+ */
+export default {
+    consumerKey: '!!!Consumer Key!!!',
+    consumerSecret: '!!!Consumer Secret!!!'
+}
